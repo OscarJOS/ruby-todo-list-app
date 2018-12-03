db02b301aa8bcc0b38f30a1e1ab4197ddbbc44c3
Elle H <itsellej@gmail.com>
2018-12-03 17:46:05 +0000

    Create edit_todo method, edit_todo.erb. User can edit a todo

--- todo.rb
+++ todo.rb
@@ -11,6 +11,11 @@ def add_new_todo(new_todo)
   }
 end
 
+def edit_todo(index, todo)
+  item_index = index.to_i - 1
+  TODOS[item_index][:todo] = todo
+end
+
 
 get "/" do
   erb :index
@@ -20,7 +25,16 @@ get "/add_todo" do
   erb :add_todo
 end
 
-post "/create" do
+post "/add_todo" do
   add_new_todo(params[:todo])
+  redirect "/"
+end
+
+get "/edit_todo" do
+  erb :edit_todo
+end
+
+post "/edit_todo" do
+  edit_todo(params[:index], params[:todo],)
   redirect "/"
 end
