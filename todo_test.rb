1781ab8c8832fa147f815a803074d379a80b4cba
itsellej <daniellehallal@gmail.com>
2018-12-04 12:21:55 +0000

    Test passed: test_it_edits_item_in_todo_list

--- todo_test.rb
+++ todo_test.rb
@@ -25,13 +25,22 @@ class ToDoTest < Test::Unit::TestCase
   #   assert last_response.body.include?('Make tea')
   # end
 
+  # def test_it_edits_item_in_todo_list
+  #   post '/add_todo', :todo => 'Make tea'
+  #   post '/edit_todo', :index => '1', :todo => 'Get milk'
+  #   get '/'
+  #   puts "***********************"
+  #   puts last_response.body
+  #   assert last_response.body.include?('Get milk')
+  # end
+
   def test_it_edits_item_in_todo_list
     post '/add_todo', :todo => 'Make tea'
-    post '/edit_todo', :index => '1', :todo => 'Get milk'
+    post '/delete_todo', :index => "1"
     get '/'
     puts "***********************"
     puts last_response.body
-    assert last_response.body.include?('Get milk')
+    assert last_response.body.include?('Your to-do list is empty')
   end
 
 
