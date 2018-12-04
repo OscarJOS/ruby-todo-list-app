5c6e891673dc253c71f528918cc0e049c3afb373
itsellej <daniellehallal@gmail.com>
2018-12-04 11:12:57 +0000

    Test passing: it_adds_item_to_todo_list

--- todo_test.rb
+++ todo_test.rb
@@ -11,10 +11,24 @@ class ToDoTest < Test::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_it_prints_empty_array
+  # def test_it_prints_empty_array
+  #   get '/'
+  #   assert last_response.ok?
+  #   assert_equal "[]\n", last_response.body
+  # end
+
+  # def test_it_shows_index_page
+  #   get '/'
+  #   puts last_response.body
+  #   assert last_response.body.include?("Let's add a new item")
+  # end
+
+  def test_it_adds_item_to_todo_list
+    post '/add_todo', :todo => 'Make tea'
     get '/'
-    assert last_response.ok?
-    assert_equal "[]\n", last_response.body
+    puts "***********************"
+    puts last_response.body
+    assert last_response.body.include?('Make tea')
   end
 
 
