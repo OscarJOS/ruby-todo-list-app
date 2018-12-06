0ac48f2df343c34fb698296be0a03bd55b2cedf6
Elle H <itsellej@gmail.com>
2018-12-06 12:25:53 +0000

    Test passing: test_it_clears_completed_todos. Update README

--- app_test.rb
+++ app_test.rb
@@ -47,11 +47,10 @@ class ToDoTest < Test::Unit::TestCase
   end
 
   def test_it_clears_completed_todos
-    post "/add_todo", :todo => "Make tea"
     post "/add_todo", :todo => "Go shopping"
     post "/complete_todo", :index => "1"
     post "/complete_todo", :index => "2"
-    post "/clear_complete_todos"
+    post "/clear_completed_todos"
     get "/"
     puts last_response.body
     assert last_response.body.include?('Your to-do list is empty')
