f777c92c79b373a1f03fc0f0f436757ddb9934e1
Elle Hallal <daniellehallal@gmail.com>
2018-12-26 15:10:30 +0000

    Test passing: edits the description of an existing a todo

--- spec/todolist_spec.rb
+++ spec/todolist_spec.rb
@@ -21,3 +21,21 @@ RSpec.describe ToDoList do
     })
   end
 end
+
+RSpec.describe ToDoList do
+
+  todo_list = nil
+  before(:each) do
+    todo_list = ToDoList.new()
+    todo_list.add_new_todo("Buy milk")
+    todo_list.add_new_todo("Pick up kids")
+  end
+
+  it "edits the description of an existing a todo" do
+    todo_list.edit_todo(0, "Food shopping")
+    expect(todo_list.todos[0][:todo]).to eq("Food shopping")
+  end
+
+
+
+end
